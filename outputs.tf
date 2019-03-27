acaff0ec9f14ef41b6f213a0378b79b4038bde79
Hadrien Chauvin <hadrien@aiden.ai>
2019-03-27 13:49:34 +0000

    improve documentation

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output log_group {
   value = "${var.log_group}"
 
   description = <<EOF
-The log group that is subscribed to. Its log events are forwarded to the syslog server.
+The name of the log group that is subscribed to. Its log events are forwarded to the syslog server.
 EOF
 }
 
 output lambda_arn {
   value       = "${aws_lambda_function.cloudwatch_to_syslog_server.arn}"
-  description = "The ARN of the lambda function used to subscribe to the log group."
+  description = "The ARN of the lambda function subscribed to the log group."
 }
--- variables.tf
+++ variables.tf
@@ -6,19 +6,19 @@ EOF
 }
 
 variable region {
-  description = "The AWS region where the logs are located."
+  description = "The AWS region where the AWS CloudWatch Logs are located."
 }
 
 variable account_id {
-  description = "The account ID where the logs are located."
+  description = "The ID of the AWS account where the AWS CloudWatch Logs are located."
 }
 
 variable log_group {
-  description = "The AWS CloudWatch log group to forward to the syslog server."
+  description = "The name of the AWS CloudWatch log group to forward to the syslog server."
 }
 
 variable syslog_server_host {
-  description = "The host for the syslog server."
+  description = "The host for the syslog server (e.g., `logs5.papertrailapp.com`)."
 }
 
 variable syslog_server_port {
