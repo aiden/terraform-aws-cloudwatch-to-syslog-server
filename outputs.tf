1fb793dc8c7a8fe5c919d1d690019155e07a91d8
Adam Kirk <atomkirk@gmail.com>
2021-03-18 07:39:57 -0600

    fixing deprecated syntax

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output log_group {
-  value = "${var.log_group}"
+  value = var.log_group
 
   description = <<EOF
 The name of the log group that is subscribed to. Its log events are forwarded to the syslog server.
@@ -7,6 +7,6 @@ EOF
 }
 
 output lambda_arn {
-  value       = "${aws_lambda_function.cloudwatch_to_syslog_server.arn}"
+  value       = aws_lambda_function.cloudwatch_to_syslog_server.arn
   description = "The ARN of the lambda function subscribed to the log group."
 }
